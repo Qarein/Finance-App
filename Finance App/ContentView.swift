8651573037669f4d178d54007a902cdec114e805
Qarein <caghandemirci@gmail.com>
2024-07-17 12:35:22 +0300

    Initial Commit

---
+++ Finance App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance App
+//
+//  Created by Çağhan Demirci on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Çağhan Demirci on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
